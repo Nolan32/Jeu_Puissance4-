9e172215d45d6e38bec82219d03e6f9048d06c10
romamaxime <roma.maxime1994@gmail.com>
2017-04-08 14:34:11 +0200

    add functions select player + array add - on

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 var counter=0;
-var result = [[], [], [], [], [], [], []];
+var result = [	[0, 0, 0, 0, 0, 0], 
+				[0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0,	0],
+				[0, 0, 0, 0, 0, 0], 
+				[0, 0, 0, 0, 0, 0], 
+				[0, 0, 0, 0, 0, 0]	];
+
+var a ;
+var row;
+var col;
+
+
+/*
 $('tbody td').click(function() {
 	counter++;
 	if (counter %2 === 0) {
@@ -9,26 +22,42 @@ $('tbody td').click(function() {
 
 	$(this).addClass('p2');
 });
+*/
+
+	$('button').click(function(){
+		counter++;		
+		counter % 2 === 0 ? a = 'p2' : a = 'p1';
+	});
+
+
+
+
+
+
 
 $('button').click(function(){
 	var button = parseInt($(this).html());
+	col = button;
 	var tableau = result;
 	for(var i =0; i<6;i++){
 
-		if(tableau[button][i] === 'p1' || tableau[button][i] === 'p2'){
+		if(tableau[button][i] === 0){
 			//tableau[button].push('a');
-			console.log(tableau);	
-			return;		
+			result = tableau;
+			//console.log(tableau);	
+			row = i;
+			console.log(row);
+			return tableau[button][i] = a;		
 		}
-			tableau[button].push('p1')
-			console.log(tableau);
-			result = tableau[button];
-			break;
 	}
-
+	return tableau;
+	$('tbody tr').eq(row).addClass('"' + a + '"');
 })
 console.log(result);
 	//console.log(tableau)
+
+
+//$('tr').eq(-row).children().eq(col).addClass('"' + a + '"');
 
 
 
