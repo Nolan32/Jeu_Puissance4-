75b26d647919d2cfdc0430dca9365fa4e17d1031
romamaxime <roma.maxime1994@gmail.com>
2017-04-11 15:18:14 +0200

    add style, alert win message and bool condition

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var counter=0;
+
 var result = [	[0, 0, 0, 0, 0, 0, 0], 
 				[0, 0, 0, 0, 0, 0, 0],
 				[0, 0, 0, 0, 0, 0, 0],
@@ -6,17 +7,38 @@ var result = [	[0, 0, 0, 0, 0, 0, 0],
 				[0, 0, 0, 0, 0, 0, 0], 
 				[0, 0, 0, 0, 0, 0, 0],	];
 
-var a ;
+var player;
 var row;
 var col;
+var bool = true;
+
+var Winner = function(a)
+{
+	if  (!a)
+	{
+		$('#youWin').append('<div class="alert alert-success" id="alert"></div>');
+		if  (player === 'p2')
+		{
+			$("#alert").append('<h1>Player 2 win</h1>');
+		}
+		else 
+		{
+			$("#alert").append('<h1>Player 1 win</h1>');
+		}
+	}
+}
 
 // fonction qui permet d'alterner tour impair => joueur 1 et tour pair => joueur 2
 
 
-	$('button').click(function(){
+$('button').click(function()
+{
+	if (bool)
+	{
 		counter++;		
-		counter % 2 === 0 ? a = 'p2' : a = 'p1';
-	});
+		counter % 2 === 0 ? player = 'p2' : player = 'p1';
+	}
+});
 
 
 
@@ -24,51 +46,70 @@ var col;
 // fonction qui stocke le choix d'un joueur dans un tableau  [[ligne1[colonne1, colonne2, etc,], [ligne2[...]], ... ]
 
 
-$('button').click(function(){
-	var button = parseInt($(this).html());
-	col = button;
-	var tableau = result;
-	for(var i = 5; i >= 0; i--){
+$('button').click(function()
+{
+	if  (bool)
+	{
+		var button = parseInt($(this).html());
+		col = button;
+		var tableau = result;
 
-		if(tableau[i][button] == 0){
-			//tableau[button].push('a');
-			//console.log(tableau);	
-			row = i;
-			//console.log(row);
-			tableau[i][button] = a;	
-			result = tableau;
-			console.log(result);
-			return;	
+		for(var i = 5; i >= 0; i--)
+		{		
+			if(tableau[i][button] == 0)
+			{
+				
+				row = i;				
+				tableau[i][button] = player;	
+				result = tableau;
+				console.log(result);
+				return;	
+			}
 		}
-	}
-	//console.log(result);
-	$('tbody tr').eq(row).addClass( a );
-})
 
-	//console.log(tableau)
+		$('tbody tr').eq(row).addClass( player );
+	}
+});
+
+	
 
 
 
 // fonction qui régénère le tableau html dynamiquement avec des background-colors pour les cases séléctionnées par les joueurs
 
-$('button').click(function(){
-	$('tbody tr').remove();
-	//console.log(result);
+$('button').click(function()
+{	
+	if  (bool)
+	{
 
-	for(var j = 0; j < result.length; j++){
-		$('tbody').append('<tr></tr>');
+		$('tbody tr').remove();
+		
 
-		for(var k = 0; k < result[j].length; k++){
-			if(result[j][k] != 0){
-				$('tbody tr').last().append('<td></td>');
-				$('tbody tr td').last().addClass(result[j][k]);
-			}
-			else {
-				$('tbody tr').last().append('<td></td>');
+		
+		for (var j = 0; j < result.length; j++)
+		{		
+
+			$('tbody').append('<tr></tr>');
+			
+			for(var k = 0; k < result[j].length; k++)
+			{			
+
+				if(result[j][k] != 0)
+				{				
+
+					$('tbody tr').last().append('<td></td>');
+					$('tbody tr td').last().addClass(result[j][k]);
+				
+
+				}	else 
+				{				
+
+					$('tbody tr').last().append('<td></td>');
+				}
 			}
 		}
 	}
-})
+});
 
 
 
@@ -77,25 +118,33 @@ $('button').click(function(){
 
 // fonction pour analyser les conditions de victoire en colonne
 
-$('button').on('click',function(){
-	
+$('button').on('click',function()
+{
+	if  (bool)
+	{
 
+		var stockVal="";
 
-	var stockVal="";
+		for(var j = 0; j <= 6; j++)
+		{
 
-for(var j = 0; j <= 6; j++){
-	for (var i = 0; i < result.length ; i++) {
-		
-			stockVal += result[i][j]; 
-			
-		}
+			for (var i = 0; i < result.length ; i++) 
+			{						
+					stockVal += result[i][j];				
 
-		if (stockVal.includes("p1p1p1p1") ) {
-			window.alert("win");
-			console.log('p1');
-		}else if (stockVal.includes("p2p2p2p2")){
-
-			console.log('p2')
+				if (stockVal.includes("p1p1p1p1") )
+				{		
+					bool = false;
+					Winner(bool);
+					return;				
+				
+				}	else if (stockVal.includes("p2p2p2p2"))
+				{					
+					bool = false;
+					Winner(bool);
+					return;
+				}
+			}
 		}
 	}
 
@@ -105,32 +154,85 @@ for(var j = 0; j <= 6; j++){
 
 // fonction pour analyser les condtitions de victoire en ligne
 
-$('button').on('click', function(){
-	
-	var stockVal = "";
-
-	for(var i = 0; i <= 5; i++){
-
-		for(var j = 0; j < result[i].length; j++){
-
-			stockVal += result[i][j];
+$('button').on('click', function()
+{	
+	if  (bool)
+	{	
+		var stockVal = "";
 		
+		for  (var i = 0; i <= 5; i++)
+		{			
+			for  (var j = 0; j < result[i].length; j++)
+			{				
+				stockVal += result[i][j];			
 
-			if(stockVal.includes("p1p1p1p1")){
-				console.log("p1");
-			} else if(stockVal.includes("p2p2p2p2")){
-				console.log('p2');
+				if  (stockVal.includes("p1p1p1p1"))
+				{				
+
+					bool = false;
+					Winner(bool);
+					return;
+				
+
+				} else if  (stockVal.includes("p2p2p2p2"))
+				{
+					bool = false;
+					Winner(bool);
+					return;
+				}
 			}
-		}
 
+		}
 	}
 });
 
+// fonction pour analyser les conditions de victoire en diagonale -> |^
+
+$('button').on('click', function()
+{
+	if  (bool)
+	{
+		var stockVal = "";
+		
+		for  (var j = -1; j <= 6; j++)
+		{
+			for  (var i = 6; i >= 0; i--)
+			{
+				j++;
+				stockVal += result[i][j];
+				console.log(stockVal);
+			
+				if  (stockVal.includes("p1p1p1p1"))
+				{
+					bool = false;
+					Winner(bool);
+					return;
+				}  
+				else if  (stockVal.includes("p2p2p2p2"))
+				{
+					bool = false;
+					Winner(bool);
+					return;
+				}
+			}
+		}
+	}
+
+	
 
 
+});
 
+//$('button').on('click', function()
+//{
 
-
+//	var stockVal += "";
+//	var j = ;
+//	for  (var i = 5; i >= 0; i--)
+//	{
+//		stock
+//	}
+//});
 
 
 
